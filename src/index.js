f98ad5a7b34312ec9dadc6e7c6a0decdbda4899a
SANTIAGO TORRALBA ALAPE <santiago.torralba@uao.edu.co>
2025-09-02 15:11:19 -0500

    agregado

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FirstApp from './FirstApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FirstApp />
+  </React.StrictMode>
+);
