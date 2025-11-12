458703b7755c1e59bf78269502412658b2a4b4f0
Pruebas de Orientacion <p.orientacion@uao.edu.co>
2025-11-12 16:45:57 -0500

    Agregado

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookStackScreen from './BookStackScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <BookStackScreen />
+    </div>
+  );
+}
+
+export default App;
